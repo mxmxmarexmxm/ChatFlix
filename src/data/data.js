bb1f7e8f0e2728afe96e0170a52605b2ee1bee88
Marinko Malencic <mare.coding@gmail.com>
2022-12-05 21:33:19 +0100

    some css frameworks added

--- src/data/data.js
+++ src/data/data.js
@@ -1,85 +1,73 @@
 export const chatsData = [
   {
     chatName: 'HTML',
-    logo:
-      'https://cdn.pixabay.com/photo/2017/08/05/11/16/image-2582748_1280.png',
+    logo: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/image-2582748_1280.png',
     tags: ['markup', 'frontend'],
   },
   {
     chatName: 'CSS',
-    logo:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png',
     tags: ['markup', 'frontend'],
   },
   {
     chatName: 'JavaScript',
-    logo:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png',
     tags: ['language', 'JS', 'frontend'],
   },
   {
     chatName: 'ReactJS',
-    logo:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--3zWuwYa3--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pdib9r9rk5j1m7oala1p.png',
+    logo: 'https://res.cloudinary.com/practicaldev/image/fetch/s--3zWuwYa3--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pdib9r9rk5j1m7oala1p.png',
     tags: ['frontend', 'frameworks', 'frontend frameworks', 'JS'],
   },
   {
     chatName: 'React Native',
-    logo:
-      'https://www.datocms-assets.com/45470/1631026680-logo-react-native.png',
-    tags: ['frontend', 'frameworks','frontend frameworks','JS', 'mobile'],
+    logo: 'https://www.datocms-assets.com/45470/1631026680-logo-react-native.png',
+    tags: ['frontend', 'frameworks', 'frontend frameworks', 'JS', 'mobile'],
   },
   {
     chatName: 'jQuery',
-    logo:
-      'https://www.nicepng.com/png/detail/274-2741860_jquery-in-easy-steps-create-dynamic-web-pages.png',
+    logo: 'https://www.nicepng.com/png/detail/274-2741860_jquery-in-easy-steps-create-dynamic-web-pages.png',
     tags: ['frontend', 'frameworks', 'frontend frameworks', 'JS'],
   },
   {
     chatName: 'NodeJS',
-    logo:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--e_rqeB7o--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://cdn-images-1.medium.com/max/2400/1%2AFPtQLT2Zk-baHficCz_mXQ.png',
-    tags: ['backend', 'frameworks','backend frameworks', 'JS'],
+    logo: 'https://res.cloudinary.com/practicaldev/image/fetch/s--e_rqeB7o--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://cdn-images-1.medium.com/max/2400/1%2AFPtQLT2Zk-baHficCz_mXQ.png',
+    tags: ['backend', 'frameworks', 'backend frameworks', 'JS'],
   },
   {
     chatName: 'AngularJS',
     logo: 'https://www.strivemindz.com/images/angularr.png',
-    tags: ['frontend', 'frameworks','frontend frameworks', 'JS'],
+    tags: ['frontend', 'frameworks', 'frontend frameworks', 'JS'],
   },
   {
     chatName: 'Vue.js',
-    logo:
-      'https://res.cloudinary.com/practicaldev/image/fetch/s--I4FWon9Y--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ll22z8mea106b4vjdjy7.jpeg',
-    tags: ['frontend', 'frameworks','frontend frameworks', 'JS'],
+    logo: 'https://res.cloudinary.com/practicaldev/image/fetch/s--I4FWon9Y--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ll22z8mea106b4vjdjy7.jpeg',
+    tags: ['frontend', 'frameworks', 'frontend frameworks', 'JS'],
   },
   {
     chatName: 'Next.js',
-    logo:
-      'https://images.ctfassets.net/23aumh6u8s0i/c04wENP3FnbevwdWzrePs/1e2739fa6d0aa5192cf89599e009da4e/nextjs',
-    tags: ['frontend', 'frameworks','frontend frameworks', 'JS'],
+    logo: 'https://images.ctfassets.net/23aumh6u8s0i/c04wENP3FnbevwdWzrePs/1e2739fa6d0aa5192cf89599e009da4e/nextjs',
+    tags: ['frontend', 'frameworks', 'frontend frameworks', 'JS'],
   },
   {
     chatName: 'Express.js',
-    logo:
-      'https://assets.website-files.com/61ca3f775a79ec5f87fcf937/6202fcdee5ee8636a145a41b_1234.png',
-    tags: ['backend', 'frameworks','backend frameworks', 'JS'],
+    logo: 'https://assets.website-files.com/61ca3f775a79ec5f87fcf937/6202fcdee5ee8636a145a41b_1234.png',
+    tags: ['backend', 'frameworks', 'backend frameworks', 'JS'],
   },
   {
     chatName: 'Flutter',
-    logo:
-      'https://ih1.redbubble.net/image.3270932181.3996/st,small,845x845-pad,1000x1000,f8f8f8.jpg',
+    logo: 'https://ih1.redbubble.net/image.3270932181.3996/st,small,845x845-pad,1000x1000,f8f8f8.jpg',
     tags: ['frontend', 'mobile'],
   },
   {
     chatName: 'Pyton',
-    logo:
-      'https://i0.wp.com/evomics.org/wp-content/uploads/2011/09/python-logo-glassy.png',
+    logo: 'https://i0.wp.com/evomics.org/wp-content/uploads/2011/09/python-logo-glassy.png',
     tags: ['backend', 'language'],
   },
   {
     chatName: 'django',
     logo: 'https://miro.medium.com/max/1200/1*1OBwwxzJksMv0YDD-XmyBw.png',
-    tags: ['backend', 'frameworks','backend frameworks', 'pyton'],
+    tags: ['backend', 'frameworks', 'backend frameworks', 'pyton'],
   },
   {
     chatName: 'Java',
@@ -88,14 +76,12 @@ export const chatsData = [
   },
   {
     chatName: 'C',
-    logo:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/695px-C_Programming_Language.svg.png',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/695px-C_Programming_Language.svg.png',
     tags: ['backend', 'language'],
   },
   {
     chatName: 'C++',
-    logo:
-      'https://www.educative.io/v2api/editorpage/5393602882568192/image/6038586442907648',
+    logo: 'https://www.educative.io/v2api/editorpage/5393602882568192/image/6038586442907648',
     tags: ['backend', 'language'],
   },
   {
@@ -110,9 +96,43 @@ export const chatsData = [
   },
   {
     chatName: 'Laravel',
-    logo:
-      'https://i.pinimg.com/736x/ab/8b/5e/ab8b5ea6637ebd8e5755c838d952b8c1.jpg',
-    tags: ['backend', 'frameworks','backend frameworks', 'php'],
+    logo: 'https://i.pinimg.com/736x/ab/8b/5e/ab8b5ea6637ebd8e5755c838d952b8c1.jpg',
+    tags: ['backend', 'frameworks', 'backend frameworks', 'php'],
+  },
+  {
+    chatName: 'Bootstrap',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png',
+    tags: ['CSS frameworks', 'frameworks', 'frontend frameworks'],
+  },
+  {
+    chatName: 'Foundation',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7vNO1jVzx7DyMUVujdIW2aQB9iojVXc0PDw&usqp=CAU',
+    tags: ['CSS frameworks', 'frameworks', 'frontend frameworks'],
+  },
+  {
+    chatName: 'Tailwind CSS',
+    logo: 'https://www.devonblog.com/wp-content/uploads/2022/06/tailwind-thumb.jpg',
+    tags: ['CSS frameworks', 'frameworks', 'frontend frameworks'],
+  },
+  {
+    chatName: 'Bulma',
+    logo: 'https://how-to.sk/wp-content/uploads/2022/08/bulma-logo.png',
+    tags: ['CSS frameworks', 'frameworks', 'frontend frameworks'],
+  },
+  {
+    chatName: 'Milligram',
+    logo: 'https://cdn.freebiesbug.com/wp-content/uploads/2016/02/milligram-framework-580x435.jpg',
+    tags: ['CSS frameworks', 'frameworks', 'frontend frameworks'],
+  },
+  {
+    chatName: 'UIkit',
+    logo: 'https://camo.githubusercontent.com/58b145be0e13ad12dea993fe9d65701d2dd580365da54f12f21e47f7288dbb7b/687474703a2f2f6f6d6e69636f64652e616d2f7069632f73657276696365732f75696b69742e706e67',
+    tags: ['CSS frameworks', 'frameworks', 'frontend frameworks'],
+  },
+  {
+    chatName: 'Materialize CSS',
+    logo: 'https://vimage.fr/vimage/img/materialize-logo.png',
+    tags: ['CSS frameworks', 'frameworks', 'frontend frameworks'],
   },
 ];
 
@@ -120,10 +140,10 @@ export const rowTitles = [
   'frontend',
   'backend',
   'JS',
-  'frameworks',
   'language',
   'markup',
+  'frameworks',
   'frontend frameworks',
   'backend frameworks',
+  'CSS frameworks',
 ];
-
