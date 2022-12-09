8d3233d33ad58bc0b58f2cd7350caff838acabc6
Marinko Malencic <mare.coding@gmail.com>
2022-12-09 20:36:57 +0100

    replay feture added to message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,11 @@
 import { useContext } from 'react';
 import { AuthContext } from '../Firebase/context';
+import { BsArrowReturnLeft } from 'react-icons/bs';
 import classes from './Message.module.css';
 
 function Message(props) {
   const { user } = useContext(AuthContext);
-  const { text, uid, photoURL } = props.message;
-
+  const { text, uid, photoURL, replayTo } = props.message;
   const messageSenderClass = uid === user?.uid ? 'sent' : 'received';
 
   return (
@@ -19,9 +19,21 @@ function Message(props) {
         />
       </div>
       <div className={classes['user-name']}>{props.message.displayName}</div>
-      <div className={classes['text-wrapper']}>
-        <p>{text}</p>
+      <div className={classes['mess-div']}>
+        {replayTo && (
+          <div className={classes['replay-wrapper']}>
+            {<p>Replayed to: {replayTo.displayName}</p>}
+            {<p>{replayTo.text}</p>}
+          </div>
+        )}
+        <div className={classes['text-wrapper']}>
+          <p>{text}</p>
+        </div>
       </div>
+      <BsArrowReturnLeft
+        className={classes.icon}
+        onClick={props.onReplay.bind(this, props.message)}
+      />
     </div>
   );
 }
