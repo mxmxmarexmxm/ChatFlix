45ec25983e2d0e6ea0dbd20ccbf662fea8678731
marinko malencic <marinko.malencic542@gmail.com>
2023-01-11 22:02:50 +0100

    Logo inserted in modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Modal.module.css';
+import logo from '../../assets/img/logo.png';
 
 const Modal = (props) => {
   if (!props.show) {
@@ -9,6 +10,9 @@ const Modal = (props) => {
   return (
     <div className={classes.dark}>
       <div className={classes.modal}>
+        <div className={classes['logo-wrapper']}>
+          <img src={logo} alt="logo" />
+        </div>
         <h2>Are you realy want to logout ? </h2>
         <div className={classes.actions}>
           <button className="toggle-button" onClick={props.onClose}>
