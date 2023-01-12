06a63c4bebeaa941b347e673574afd06885d2c65
marinko malencic <marinko.malencic542@gmail.com>
2023-01-13 00:09:18 +0100

    Logic impoved

--- src/components/ChatRow.js
+++ src/components/ChatRow.js
@@ -3,39 +3,45 @@ import classes from './ChatRow.module.css';
 import { FaGreaterThan, FaLessThan } from 'react-icons/fa';
 
 const ChatRow = (props) => {
-  const div = useRef();
+  const chatRowRef = useRef();
 
-  const scroll = (scrollOffset) => {
-    div.current.scrollLeft += scrollOffset;
+  let haveScrool = chatRowRef.current?.scrollLeftMax > 0;
+
+  const chats = props.chats.filter((chat) => chat.tags.includes(props.rowTitle));
+
+  const scrollRowHandler = (scrollOffset) => {
+    chatRowRef.current.scrollLeft += scrollOffset;
   };
 
   useEffect(() => {
-    scroll(-1000);
+    scrollRowHandler(-1000);
   }, []);
 
   return (
-    <div className={classes.container}>
+    <div className={classes.chatRow}>
       <h2>{props.rowTitle}</h2>
       <div className={classes.row}>
-        <button onClick={() => scroll(-200)}>
-          <FaLessThan />
-        </button>
-        <div ref={div} className={classes.scroll}>
-          {props.chats
-            .filter((chat) => chat.tags.includes(props.rowTitle))
-            .map((chat) => (
-              <img
-                className={classes['logo']}
-                onClick={props.onSelectChat.bind(this, chat)}
-                src={chat.logo}
-                alt={chat.chatName}
-                key={chat.chatName}
-              />
-            ))}
+        {haveScrool && (
+          <button onClick={() => scrollRowHandler(-200)}>
+            <FaLessThan />
+          </button>
+        )}
+        <div ref={chatRowRef} className={classes.chats}>
+          {chats.map((chat) => (
+            <img
+              className={classes.logo}
+              onClick={props.onSelectChat.bind(this, chat)}
+              src={chat.logo}
+              alt={chat.chatName}
+              key={chat.id}
+            />
+          ))}
         </div>
-        <button onClick={() => scroll(200)}>
-          <FaGreaterThan />
-        </button>
+        {haveScrool && (
+          <button onClick={() => scrollRowHandler(200)}>
+            <FaGreaterThan />
+          </button>
+        )}
       </div>
     </div>
   );
