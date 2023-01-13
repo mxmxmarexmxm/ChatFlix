b52cf67a5b9c07b4557ff169e8964c35d6cef514
marinko malencic <marinko.malencic542@gmail.com>
2023-01-13 02:37:07 +0100

    Open modal instead of alert

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import classes from './Chat.module.css';
+import Modal from '../UI/Modal';
 
 // Icons
 import { AiOutlineClose, AiOutlineFullscreen, AiOutlineFullscreenExit } from 'react-icons/ai';
@@ -11,7 +12,7 @@ import SideChatFullScreen from '../SideChatFullScreen';
 import Message from '../Message';
 import useSound from 'use-sound';
 
-import notificationSound from '../../assets/Sound/notification-sound.mp3'
+import notificationSound from '../../assets/Sound/notification-sound.mp3';
 
 // Firebase
 import firebase from '../../Firebase/Firebase';
@@ -93,7 +94,7 @@ const Chat = (props) => {
       dummy.current.scrollIntoView({ behavior: 'smooth' });
       setMessageText('');
     } else {
-      alert('You must be signed in to send message!');
+      props.onTryToSend();
     }
     setMessageToReplay(null);
   };
@@ -117,7 +118,7 @@ const Chat = (props) => {
   useEffect(() => {
     const handleEsc = (event) => {
       if (event.keyCode === 27) {
-        setShowChatMessages(false)
+        setShowChatMessages(false);
       }
     };
     window.addEventListener('keydown', handleEsc);
@@ -140,7 +141,7 @@ const Chat = (props) => {
   let chatBodyClass = showChatMessages ? classes['chat-body'] : classes['hide-body'];
   let chatClass = classes.chat;
   let messagesContainerClass = classes['messages-container'];
-  let chatHeadClass= classes['chat-head']
+  let chatHeadClass = classes['chat-head'];
 
   // Full screen chat
   if (props.isFullScreen) {
@@ -149,7 +150,7 @@ const Chat = (props) => {
       classes[`messages-container-full`]
     }`;
     chatBodyClass = `${classes['chat-body-full']}`;
-    chatHeadClass= `${classes['chat-head']} ${classes['chat-head-full']}`
+    chatHeadClass = `${classes['chat-head']} ${classes['chat-head-full']}`;
   }
 
   // Side chat on full screen
@@ -178,64 +179,70 @@ const Chat = (props) => {
   }
 
   return (
-    <div className={chatClass}>
-      <div className={chatHeadClass} onClick={showChatHandler}>
-        <div className={classes.logoAndTitle}>
-          <div className={classes['chat-head-image-container']}>
-            <img src={logo} alt="avatar" />
-          </div>
-          <h2>{chatName}</h2>
-        </div>
-        <div className={classes[`button-wrapper`]}>
-          {!showChatMessages && unreadMessages > 0 && (
-            <div className={classes[`unread-messages-badge`]}>{unreadMessages}</div>
-          )}
-          <div className={classes['icon-btn-wrapper']} onClick={maximizeHandler}>
-            {props.isFullScreen ? (
-              <AiOutlineFullscreenExit className={classes.icon} />
-            ) : (
-              <AiOutlineFullscreen className={classes.icon} />
-            )}
-          </div>
-          <div className={classes['icon-btn-wrapper']} onClick={props.onClose.bind(this, chatName)}>
-            <AiOutlineClose className={classes.icon} />
-          </div>
-        </div>
-      </div>
-      <div className={chatBodyClass}>
-        <div className={messagesContainerClass}>
-          {loading ? (
-            <p className={classes['empty-chat']}>Loading...</p>
-          ) : messages?.length > 0 ? (
-            messages.map((message) => (
-              <Message key={message.id} message={message} onReplay={onReplayHandler} />
-            ))
-          ) : (
-            <p className={classes['empty-chat']}>
-              There are no messages yet! <br />
-              Start conversation!
-            </p>
-          )}
-          <span ref={dummy}></span>
-        </div>
-        {messageToReplay && (
-          <div className={classes['message-to-replay']}>
-            <div className={classes['replay-texts']}>
-              <p>Replying to {messageToReplay.displayName}</p>
-              <p className={classes['replay-text']}>{messageToReplay.text}</p>
+    <>
+      <Modal show={true} />
+      <div className={chatClass}>
+        <div className={chatHeadClass} onClick={showChatHandler}>
+          <div className={classes.logoAndTitle}>
+            <div className={classes['chat-head-image-container']}>
+              <img src={logo} alt="avatar" />
             </div>
-            <AiOutlineClose className={classes.icon} onClick={() => setMessageToReplay(null)} />
+            <h2>{chatName}</h2>
           </div>
-        )}
-        <form onSubmit={sendMessage}>
-          <input
-            value={messageText}
-            onClick={markAllAsRead}
-            onChange={(e) => setMessageText(e.target.value)}
-          />
-        </form>
+          <div className={classes[`button-wrapper`]}>
+            {!showChatMessages && unreadMessages > 0 && (
+              <div className={classes[`unread-messages-badge`]}>{unreadMessages}</div>
+            )}
+            <div className={classes['icon-btn-wrapper']} onClick={maximizeHandler}>
+              {props.isFullScreen ? (
+                <AiOutlineFullscreenExit className={classes.icon} />
+              ) : (
+                <AiOutlineFullscreen className={classes.icon} />
+              )}
+            </div>
+            <div
+              className={classes['icon-btn-wrapper']}
+              onClick={props.onClose.bind(this, chatName)}
+            >
+              <AiOutlineClose className={classes.icon} />
+            </div>
+          </div>
+        </div>
+        <div className={chatBodyClass}>
+          <div className={messagesContainerClass}>
+            {loading ? (
+              <p className={classes['empty-chat']}>Loading...</p>
+            ) : messages?.length > 0 ? (
+              messages.map((message) => (
+                <Message key={message.id} message={message} onReplay={onReplayHandler} />
+              ))
+            ) : (
+              <p className={classes['empty-chat']}>
+                There are no messages yet! <br />
+                Start conversation!
+              </p>
+            )}
+            <span ref={dummy}></span>
+          </div>
+          {messageToReplay && (
+            <div className={classes['message-to-replay']}>
+              <div className={classes['replay-texts']}>
+                <p>Replying to {messageToReplay.displayName}</p>
+                <p className={classes['replay-text']}>{messageToReplay.text}</p>
+              </div>
+              <AiOutlineClose className={classes.icon} onClick={() => setMessageToReplay(null)} />
+            </div>
+          )}
+          <form onSubmit={sendMessage}>
+            <input
+              value={messageText}
+              onClick={markAllAsRead}
+              onChange={(e) => setMessageText(e.target.value)}
+            />
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
