69eee6301998957a22f46ff9ad8481688ffd596c
marinko malencic <marinko.malencic542@gmail.com>
2023-02-15 14:35:12 +0100

    Sign out with async/await

--- src/auth/AuthServices.js
+++ src/auth/AuthServices.js
@@ -2,12 +2,19 @@ import firebase from '../Firebase/Firebase';
 
 let auth = firebase.auth();
 
-export  const signInWithGoogle = () => {
+export const signInWithGoogle = async () => {
   auth = firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
   auth.signInWithPopup(provider);
 };
 
-export const signOut = () => {
-  auth.signOut();
+export const signOut = async () => {
+  try {
+    const succes = auth.signOut();
+    if (succes) {
+      alert('You have been logged out successfully!');
+    }
+  } catch (err) {
+    alert(err.message);
+  }
 };
