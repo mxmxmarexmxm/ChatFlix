052bbf4308bb214af57a289d4b5fc5f135ffffe2
marinko malencic <marinko.malencic542@gmail.com>
2023-09-14 21:57:20 +0200

    feat: add drop down menu to the header

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,7 +1,6 @@
-// Header.js
 import classes from './Header.module.css';
 import { useModal } from '../../context/ModalContext';
-import { useContext } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { AuthContext } from '../../Firebase/context';
 import logo from '../../assets/img/logo.png';
 import { signOut } from '../../auth/AuthServices';
@@ -9,44 +8,72 @@ import userPlaceholder from '../../assets/img/user-placeholder.png';
 
 const Header = () => {
   const { openModal, closeModal } = useModal();
+  const [openMenu, setOpenMenu] = useState(false);
   const { user } = useContext(AuthContext);
+  const menuRef = useRef(null);
 
   const signOutHandler = () => {
     signOut();
     closeModal();
   };
 
+  // Close menu when a click occurs outside the menu
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setOpenMenu(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <>
-      <header>
-        <div>
-          <a href="/ChatFlix">
-            <img src={logo} alt="logo" />
-          </a>
-        </div>
-        {user ? (
-          <div className={classes['sign-out-container']}>
-            <button
-              className={classes['sign-btn']}
-              onClick={() => signOutHandler()}
-            >
-              Sign Out
-            </button>
-            <div className={classes['image-wrapper']}>
-              <img
-                alt="profile avatar"
-                src={user.photoURL || userPlaceholder}
-                referrerPolicy="no-referrer"
-              />
-            </div>
+    <header>
+      <div>
+        <a href="/ChatFlix">
+          <img src={logo} alt="logo" />
+        </a>
+      </div>
+      {user ? (
+        <div className={classes['drop-menu']} ref={menuRef}>
+          <div
+            className={classes['image-wrapper']}
+            onClick={() => setOpenMenu(true)}
+          >
+            <img
+              alt="profile avatar"
+              src={user.photoURL || userPlaceholder}
+              referrerPolicy="no-referrer"
+            />
           </div>
-        ) : (
-          <button className={classes['sign-btn']} onClick={openModal}>
-            Sign In
-          </button>
-        )}
-      </header>
-    </>
+          {openMenu && (
+            <ul>
+              <li>Example</li>
+              <li>Example</li>
+              <li>Example</li>
+              <li>Example</li>
+              <li>
+                <button
+                  className={classes['sign-btn']}
+                  onClick={() => signOutHandler()}
+                >
+                  Sign Out
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+      ) : (
+        <button className={classes['sign-btn']} onClick={openModal}>
+          Sign In
+        </button>
+      )}
+    </header>
   );
 };
 
