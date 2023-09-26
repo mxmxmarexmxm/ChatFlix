c84784a57f3bddb2a0c1dd415d3fe28dfc7c41ef
marinko malencic <marinko.malencic542@gmail.com>
2023-09-26 22:07:14 +0200

    feat: close modal on esc key

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useModal } from '../../context/ModalContext';
 import classes from './Modal.module.css';
 import { Close } from '../../assets/icons/Close';
 
 const Modal = () => {
   const { modalVisible, closeModal, modalContent } = useModal();
+
+  useEffect(() => {
+    const handleEsc = (event) => {
+      if (event.keyCode === 27) {
+        closeModal();
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, [closeModal]);
 
   return (
     modalVisible && (
