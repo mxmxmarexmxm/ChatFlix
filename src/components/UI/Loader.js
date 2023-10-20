aaefaec84608daef1b198b6e33e4e8234a0dbe86
marinko malencic <marinko.malencic542@gmail.com>
2023-10-20 18:39:31 +0200

    style: add wrapper to center loader

--- src/components/UI/Loader.js
+++ src/components/UI/Loader.js
@@ -2,7 +2,11 @@ import React from 'react';
 import classes from './Loader.module.css';
 
 const Loader = () => {
-  return <div className={classes['loader']}></div>;
+  return (
+    <div className={classes['loader-wrapper']}>
+      <div className={classes['loader']}></div>
+    </div>
+  );
 };
 
 export default Loader;
