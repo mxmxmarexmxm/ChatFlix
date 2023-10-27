e029f21180017f4c2a3ea3c1b82f24ec2c8ddf83
marinko malencic <marinko.malencic542@gmail.com>
2023-10-27 21:57:45 +0200

    style: improve reactions preview ui

--- src/components/UI/ReactionsPreview.js
+++ src/components/UI/ReactionsPreview.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { reactionsIconsArray } from '../Message/MessageUtils';
+import classes from './ReactionsPreview.module.css';
 
 // Improve UI, add users listing, fetch users data, sort by size ...
 const ReactionsPreview = ({ reactions }) => {
   return (
-    <div>
+    <div className={classes['reactions-container']}>
       {Object.entries(reactions)
         .filter(([, users]) => users.length > 0)
         .map(([reaction, users]) => (
-          <div key={reaction}>
-            {users.length} {reactionsIconsArray[reaction]}
+          <div key={reaction} className={classes['reaction-counter']}>
+            <span>{users.length}</span> {reactionsIconsArray[reaction]}
           </div>
         ))}
     </div>
