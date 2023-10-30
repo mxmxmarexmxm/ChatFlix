978db7fd5414bf5411e004924830142bc07f41ef
marinko malencic <marinko.malencic542@gmail.com>
2023-10-30 23:29:42 +0100

    feat: user can preview other users profile on click on reaction

--- src/components/UI/ReactionsPreview.js
+++ src/components/UI/ReactionsPreview.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { reactionsIconsArray } from '../Message/MessageUtils';
 import classes from './ReactionsPreview.module.css';
+import { useModal } from '../../context/ModalContext';
 import { getUserDataFromFirestore } from '../../services/UserServices';
+import UserProfile from '../UserProfile';
 
 const ReactionsPreview = ({ reactions }) => {
   const [userReactions, setUserReactions] = useState([]);
   const [totalUsers, setTotalUsers] = useState(0);
+  const { openModal } = useModal();
 
   useEffect(() => {
-    // Fetch user data and populate userReactions array
     const fetchUserReactions = async () => {
       let userReactionsData = [];
       for (const reaction in reactions) {
         for (const user of reactions[reaction]) {
-          const userData = await getUserDataFromFirestore(user); // Fetch user data
+          const userData = await getUserDataFromFirestore(user);
           if (userData) {
             userReactionsData.push({
-              user: userData, // Replace with the appropriate user data
+              displayName: userData.displayName,
               reaction,
+              uid: user,
             });
           }
         }
@@ -33,9 +36,13 @@ const ReactionsPreview = ({ reactions }) => {
     <div className={classes['reactions-preview']}>
       <div className={classes['total-users']}>Total Users: {totalUsers}</div>
       <div className={classes['reactions-wrapper']}>
-        {userReactions.map(({ user, reaction }, index) => (
-          <div key={index} className={classes['reaction-wrapper']}>
-            <span>{user.displayName}</span> {reactionsIconsArray[reaction]}
+        {userReactions.map(({ displayName, reaction, uid }, index) => (
+          <div
+            key={index}
+            className={classes['reaction-wrapper']}
+            onClick={() => openModal(<UserProfile uid={uid} />)}
+          >
+            <span>{displayName}</span> {reactionsIconsArray[reaction]}
           </div>
         ))}
       </div>
