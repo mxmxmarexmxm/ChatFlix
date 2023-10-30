33f7abf40f3d9f25a06233f13f762f2231426809
marinko malencic <marinko.malencic542@gmail.com>
2023-10-30 23:15:28 +0100

    feat: user can view user by name at reaction preview

--- src/components/UI/ReactionsPreview.js
+++ src/components/UI/ReactionsPreview.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { reactionsIconsArray } from '../Message/MessageUtils';
 import classes from './ReactionsPreview.module.css';
+import { getUserDataFromFirestore } from '../../services/UserServices';
 
-// Improve UI, add users listing, fetch users data, sort by size ...
 const ReactionsPreview = ({ reactions }) => {
+  const [userReactions, setUserReactions] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+
+  useEffect(() => {
+    // Fetch user data and populate userReactions array
+    const fetchUserReactions = async () => {
+      let userReactionsData = [];
+      for (const reaction in reactions) {
+        for (const user of reactions[reaction]) {
+          const userData = await getUserDataFromFirestore(user); // Fetch user data
+          if (userData) {
+            userReactionsData.push({
+              user: userData, // Replace with the appropriate user data
+              reaction,
+            });
+          }
+        }
+      }
+      setUserReactions(userReactionsData);
+      setTotalUsers(userReactionsData.length);
+    };
+
+    fetchUserReactions();
+  }, [reactions]);
+
   return (
-    <div className={classes['reactions-container']}>
-      {Object.entries(reactions)
-        .filter(([, users]) => users.length > 0)
-        .map(([reaction, users]) => (
-          <div key={reaction} className={classes['reaction-counter']}>
-            <span>{users.length}</span> {reactionsIconsArray[reaction]}
+    <div className={classes['reactions-preview']}>
+      <div className={classes['total-users']}>Total Users: {totalUsers}</div>
+      <div className={classes['reactions-wrapper']}>
+        {userReactions.map(({ user, reaction }, index) => (
+          <div key={index} className={classes['reaction-wrapper']}>
+            <span>{user.displayName}</span> {reactionsIconsArray[reaction]}
           </div>
         ))}
+      </div>
     </div>
   );
 };
