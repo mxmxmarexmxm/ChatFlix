835269867b3c9714844b524b936f6325adfba297
marinko malencic <marinko.malencic542@gmail.com>
2023-10-31 22:07:42 +0100

    feat: user can filter reactions in reactions preview

--- src/components/UI/ReactionsPreview.js
+++ src/components/UI/ReactionsPreview.js
@@ -9,6 +9,7 @@ import userPlaceholder from '../../assets/img/user-placeholder.png';
 
 const ReactionsPreview = ({ reactions }) => {
   const [userReactions, setUserReactions] = useState([]);
+  const [reactionFilter, setReactionFilter] = useState(null);
   const [totalUsers, setTotalUsers] = useState(0);
   const [loading, setLoading] = useState(false);
   const { openModal } = useModal();
@@ -39,16 +40,39 @@ const ReactionsPreview = ({ reactions }) => {
     fetchUserReactions();
   }, [reactions]);
 
+  const nonEmptyReactions = Object.entries(reactions).filter(
+    (reaction) => reaction[1].length !== 0
+  );
+
   if (loading) {
     return <Loader />;
   }
 
   return (
     <div className={classes['reactions-preview']}>
-      <div className={classes['total-users']}>Total Users: {totalUsers}</div>
+      <div className={classes['reactions-filter-menu']}>
+        <div
+          className={classes['total-users']}
+          onClick={() => setReactionFilter(null)}
+        >
+          All: {totalUsers}
+        </div>
+        {nonEmptyReactions.map((reaction) => (
+          <div
+            className={classes['reactions-filter']}
+            onClick={() => setReactionFilter(reaction[0])}
+          >
+            {reaction[1].length}
+            {reactionsIconsArray[reaction[0]]}
+          </div>
+        ))}
+      </div>
       <div className={classes['reactions-wrapper']}>
-        {userReactions.map(
-          ({ displayName, reaction, photoURL, uid }, index) => (
+        {userReactions
+          .filter((reaction) =>
+            reactionFilter ? reaction.reaction === reactionFilter : reaction
+          )
+          .map(({ displayName, reaction, photoURL, uid }, index) => (
             <div key={index} className={classes['reaction-wrapper']}>
               <div
                 className={classes['name-photo-wrapper']}
@@ -61,8 +85,7 @@ const ReactionsPreview = ({ reactions }) => {
               </div>
               {reactionsIconsArray[reaction]}
             </div>
-          )
-        )}
+          ))}
       </div>
     </div>
   );
