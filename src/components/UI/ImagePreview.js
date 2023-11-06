fb12ef66f810c07a5e64209a44ba3d7acafb6c19
marinko malencic <marinko.malencic542@gmail.com>
2023-11-06 22:36:56 +0100

    feat: add next/previous buttons for gallery preview

--- src/components/UI/ImagePreview.js
+++ src/components/UI/ImagePreview.js
@@ -2,19 +2,40 @@ import React, { useState } from 'react';
 import classes from './ImagePreview.module.css';
 
 const ImagePreview = ({ url, gallery }) => {
-  const [currentUrl, setCurrentUrl] = useState(url);
+  const [currentUrlIndex, setCurrentUrlIndex] = useState(null);
 
   return (
     <div className={classes['image-preview']}>
       <div className={classes['image-preview-wrapper']}>
-        <img src={currentUrl} alt="photo-preview" />
+        <img
+          src={currentUrlIndex ? gallery[currentUrlIndex] : url}
+          alt="Previewed Image"
+        />
       </div>
       <div className={classes.gallery}>
-        {gallery?.map((photo, index) => (
-          <div onClick={() => setCurrentUrl(photo)} key={index}>
-            <img src={photo} alt="photo" />
+        {gallery?.map((photoUrl, index) => (
+          <div onClick={() => setCurrentUrlIndex(index)} key={index}>
+            <img
+              src={photoUrl}
+              alt={`Thumbnail ${index}`}
+              onClick={() => setCurrentUrlIndex(index)}
+            />
           </div>
         ))}
+      </div>
+      <div className={classes.controls}>
+        <button
+          onClick={() => setCurrentUrlIndex((index) => index - 1)}
+          disabled={currentUrlIndex === 0}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setCurrentUrlIndex((index) => index + 1)}
+          disabled={currentUrlIndex === gallery?.length - 1}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
