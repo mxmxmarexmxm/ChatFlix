7438ff33dbb1ac18d3df9f09151bda2dc4cebe6b
marinko malencic <marinko.malencic542@gmail.com>
2023-11-07 17:45:02 +0100

    feat: handle keyboard commands for navigating images

--- src/components/UI/ImagePreview.js
+++ src/components/UI/ImagePreview.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './ImagePreview.module.css';
 import { RightArrow } from '../../assets/icons/RightArrow';
 import { LeftArrow } from '../../assets/icons/LeftArrow';
 
 const ImagePreview = ({ url, gallery }) => {
   const [currentUrlIndex, setCurrentUrlIndex] = useState(null);
+
+  // Check if the currently displayed image is the first or last in the gallery
+  const isFirstImage = currentUrlIndex === 0 || gallery?.length === 1;
+  const isLastImage =
+    currentUrlIndex === gallery?.length - 1 || gallery?.length === 1;
+
+  // Handle keyboard commands for navigating images
+  useEffect(() => {
+    const handleArrowCommands = (event) => {
+      if (event.keyCode === 37 && !isFirstImage) {
+        setCurrentUrlIndex((index) => index - 1);
+      } else if (event.keyCode === 39 && !isLastImage) {
+        setCurrentUrlIndex((index) => index + 1);
+      }
+    };
+    window.addEventListener('keydown', handleArrowCommands);
+
+    return () => {
+      window.removeEventListener('keydown', handleArrowCommands);
+    };
+  }, [isFirstImage, isLastImage]);
 
   return (
     <div className={classes['image-preview']}>
@@ -34,15 +55,13 @@ const ImagePreview = ({ url, gallery }) => {
       <div className={classes.controls}>
         <button
           onClick={() => setCurrentUrlIndex((index) => index - 1)}
-          disabled={currentUrlIndex === 0 || gallery?.length === 1}
+          disabled={isFirstImage}
         >
           <LeftArrow />
         </button>
         <button
           onClick={() => setCurrentUrlIndex((index) => index + 1)}
-          disabled={
-            currentUrlIndex === gallery?.length - 1 || gallery?.length === 1
-          }
+          disabled={isLastImage}
         >
           <RightArrow />
         </button>
