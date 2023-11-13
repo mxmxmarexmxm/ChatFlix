014368c9286cf1364412f660a5bbcfefa3cff811
marinko malencic <marinko.malencic542@gmail.com>
2023-11-13 21:00:39 +0100

    refactor: Introduce isNotEmpty check for chats length, preventing rendering ChatRow with 0 fields

--- src/components/ChatRow.js
+++ src/components/ChatRow.js
@@ -17,19 +17,22 @@ const ChatRow = ({ rowTitle, onSelectChat }) => {
   const chats = [...chatsData, ...favoriteChats].filter((chat) =>
     chat.tags.includes(rowTitle)
   );
+  
+  const isNotEmpty = chats?.length !== 0;
 
   const scrollRowHandler = (scrollOffset) => {
     chatRowRef.current.scrollLeft += scrollOffset;
   };
 
   const checkHorizontalOverflow = () => {
-    setHaveScrool(
-      chatRowRef.current.scrollWidth > chatRowRef.current.clientWidth
-    );
+    isNotEmpty &&
+      setHaveScrool(
+        chatRowRef.current.scrollWidth > chatRowRef.current.clientWidth
+      );
   };
 
   useEffect(() => {
-    scrollRowHandler(-1000);
+    isNotEmpty && scrollRowHandler(-1000);
     checkHorizontalOverflow();
 
     window.addEventListener('resize', checkHorizontalOverflow);
@@ -40,48 +43,52 @@ const ChatRow = ({ rowTitle, onSelectChat }) => {
   }, []);
 
   return (
-    <div className={classes['chat-row-container']}>
-      <h2>{rowTitle}</h2>
-      <div className={classes['row-body']}>
-        <button
-          onClick={() => scrollRowHandler(isMobile ? -200 : -900)}
-          className={`${!haveScrool && classes['hide-button']}`}
-          aria-label="Scroll Left"
-        >
-          {haveScrool && <LeftArrow />}
-        </button>
-        <div className={classes['chats-outer-wrapper']}>
-          <div ref={chatRowRef} className={classes['chats-inner-wrapper']}>
-            {chats.map((chat) => (
-              <div className={classes['logo-container']} key={chat.id}>
-                <img
-                  onClick={() => onSelectChat(chat)}
-                  src={chat.logo}
-                  alt={chat.name}
-                  {...(![
-                    'frontend',
-                    'frontend frameworks',
-                    'CSS frameworks',
-                  ].includes(rowTitle) && { loading: 'lazy' })}
-                  onError={(e) => {
-                    e.target.src = placeholder;
-                  }}
-                  onLoad={checkHorizontalOverflow}
-                />
+    <>
+      {isNotEmpty && (
+        <div className={classes['chat-row-container']}>
+          <h2>{rowTitle}</h2>
+          <div className={classes['row-body']}>
+            <button
+              onClick={() => scrollRowHandler(isMobile ? -200 : -900)}
+              className={`${!haveScrool && classes['hide-button']}`}
+              aria-label="Scroll Left"
+            >
+              {haveScrool && <LeftArrow />}
+            </button>
+            <div className={classes['chats-outer-wrapper']}>
+              <div ref={chatRowRef} className={classes['chats-inner-wrapper']}>
+                {chats.map((chat) => (
+                  <div className={classes['logo-container']} key={chat.id}>
+                    <img
+                      onClick={() => onSelectChat(chat)}
+                      src={chat.logo}
+                      alt={chat.name}
+                      {...(![
+                        'frontend',
+                        'frontend frameworks',
+                        'CSS frameworks',
+                      ].includes(rowTitle) && { loading: 'lazy' })}
+                      onError={(e) => {
+                        e.target.src = placeholder;
+                      }}
+                      onLoad={checkHorizontalOverflow}
+                    />
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+
+            <button
+              onClick={() => scrollRowHandler(isMobile ? 200 : 900)}
+              className={`${!haveScrool && classes['hide-button']}`}
+              aria-label="Scroll Right"
+            >
+              {haveScrool && <RightArrow />}
+            </button>
           </div>
         </div>
-
-        <button
-          onClick={() => scrollRowHandler(isMobile ? 200 : 900)}
-          className={`${!haveScrool && classes['hide-button']}`}
-          aria-label="Scroll Right"
-        >
-          {haveScrool && <RightArrow />}
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
