95b2129044d0499ac49ff10d1ab6ae5d0c890913
marinko malencic <marinko.malencic542@gmail.com>
2023-11-15 01:57:00 +0100

    feat: Display CopySuccess preview for 3 seconds after successful code copy

--- src/components/UI/CodeSnippet.js
+++ src/components/UI/CodeSnippet.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Highlight, themes } from 'prism-react-renderer';
 import { Copy } from '../../assets/icons/Copy';
 import classes from './CodeSnippet.module.css';
+import { CopySuccess } from '../../assets/icons/CopySuccess';
 
 const CodeSnippet = ({ code, language }) => {
+  const [successfulCopy, setSuccessfulCopy] = useState(false);
+
+  useEffect(() => {
+    if (successfulCopy) {
+      setTimeout(() => setSuccessfulCopy(false), 3000);
+    }
+  }, [successfulCopy]);
+
+  const handleClipboardCopy = () => {
+    setSuccessfulCopy(true);
+    navigator.clipboard.writeText(code);
+  };
+
   return (
     <div className={classes['code-snippet']}>
       <button
-        className={classes['copy-btn']}
-        onClick={() => navigator.clipboard.writeText(code)}
+        className={`${classes['copy-btn']} ${
+          successfulCopy ? classes['successful-copy'] : ''
+        }`}
+        onClick={handleClipboardCopy}
       >
-        <Copy />
+        {successfulCopy ? <CopySuccess /> : <Copy />}
       </button>
       <Highlight
         theme={themes.vsDark}
