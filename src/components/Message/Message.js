c4acdcf5e9544260ecf4b8ae1511ecce4239c23d
marinko malencic <marinko.malencic542@gmail.com>
2023-11-16 20:17:27 +0100

    chore: remove unused import

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -15,7 +15,6 @@ import userPlaceholder from '../../assets/img/user-placeholder.png';
 import { Replay } from '../../assets/icons/Replay';
 import { Reaction } from '../../assets/icons/Reaction';
 import { reactionsIconsArray } from './MessageUtils';
-import { Copy } from '../../assets/icons/Copy';
 import ReactionsPreview from '../UI/ReactionsPreview';
 import useClickOutside from '../../hooks/useClickOutside';
 import AuthForm from '../../auth/AuthForm';
