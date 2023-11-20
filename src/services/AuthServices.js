d971fd11c73d5e2525504a4b71f34850e42e3a46
marinko malencic <marinko.malencic542@gmail.com>
2023-11-20 21:10:58 +0100

    chore: remove fb login option

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,10 +1,5 @@
 import firebase from '../Firebase/Firebase';
-import {
-  sendPasswordResetEmail,
-  FacebookAuthProvider,
-  getAuth,
-  signInWithPopup,
-} from 'firebase/auth';
+import { sendPasswordResetEmail } from 'firebase/auth';
 let auth = firebase.auth();
 
 export const signInWithGoogle = async () => {
@@ -18,36 +13,6 @@ export const signInWithGoogle = async () => {
     alert(err.message);
     return null;
   }
-};
-
-export const signInWithFacebook = async () => {
-  const provider = new FacebookAuthProvider();
-  const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // The signed-in user info.
-      const user = result.user;
-
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      const credential = FacebookAuthProvider.credentialFromResult(result);
-      const accessToken = credential.accessToken;
-
-      console.log(user);
-
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = FacebookAuthProvider.credentialFromError(error);
-      console.log(errorMessage);
-      // ...
-    });
 };
 
 export const signOut = async () => {
