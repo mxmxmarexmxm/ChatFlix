8954c65bf6fd3b4cdacbfa767e81dad44bc3b93b
marinko malencic <marinko.malencic542@gmail.com>
2023-11-21 23:07:54 +0100

    chore: add defult height to close icon

--- src/assets/icons/Close.js
+++ src/assets/icons/Close.js
@@ -1,6 +1,11 @@
 export const Close = ({ ...props }) => {
   return (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      height="20"
+      {...props}
+    >
       <title>Close</title>
       <g id="Menu / Close_LG">
         <path
