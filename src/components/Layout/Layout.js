260f47b02229726bb6e1d26849572b635072eeae
marinko malencic <marinko.malencic542@gmail.com>
2023-11-30 15:57:13 +0100

    style: configure general layout for light/dark mode

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,21 @@ import classes from './Layout.module.css';
 import { useChatContext } from '../../context/ChatContext';
 import FullScreen from '../../screens/FullScreen';
 import HomeScreen from '../../screens/HomeScreen';
+import { useSettingsContext } from '../../context/SettingsContext';
 
 const Layout = () => {
+  const { settings } = useSettingsContext();
   const { fullScreenChat } = useChatContext();
   const content = fullScreenChat ? <FullScreen /> : <HomeScreen />;
-
-  return <div className={classes.layout}>{content}</div>;
+  return (
+    <div
+      className={`${classes.layout} ${
+        !settings.darkMode ? classes['light-mode'] : ''
+      }`}
+    >
+      {content}
+    </div>
+  );
 };
 
 export default Layout;
