1718f62be2d8a4872c4c0d860505c0a3fe6bfbc7
marinko malencic <marinko.malencic542@gmail.com>
2024-01-03 18:38:59 +0100

    chore: add show and hide password titles to the icons

--- src/assets/icons/Eye.js
+++ src/assets/icons/Eye.js
@@ -6,6 +6,7 @@ export const Eye = ({ ...props }) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
+      <title>Show Password</title>
       <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
--- src/assets/icons/EyeOff.js
+++ src/assets/icons/EyeOff.js
@@ -6,6 +6,7 @@ export const EyeOff = ({ ...props }) => {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
+      <title>Hide Password</title>
       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
