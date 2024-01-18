b62c64bed3d4581a2b5a462a3b145b713cf367aa
marinko malencic <marinko.malencic542@gmail.com>
2024-01-18 04:57:40 +0100

    Committing changes

---
+++ src/components/ChatRow2.js
@@ -0,0 +1,13 @@
+import classes from './ChatRow2.module.css';
+
+const ChatRow2 = () => {
+  return (
+    <div className={classes.screen}>
+      <div className={classes.inner}>
+        <input />
+      </div>
+    </div>
+  );
+};
+
+export default ChatRow2;
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -14,6 +14,7 @@ import { Close } from '../../assets/icons/Close';
 import { Profile } from '../../assets/icons/Profile';
 import { LogOutIcon } from '../../assets/icons/LogOutIcon';
 import { Sound } from '../../assets/icons/Sound';
+import { CodeVector } from '../../assets/icons/CodeVector';
 import Toggle from './Toggle';
 import { useSettingsContext } from '../../context/SettingsContext';
 import CodeSettings from './CodeThemeSettings';
@@ -116,7 +117,7 @@ const Header = ({ setSearchTerm, searchTerm }) => {
                   onClick={() => openModal(<CodeSettings />)}
                   aria-label="Code Theme"
                 >
-                  <Profile />
+                  <CodeVector />
                   Code Theme
                 </button>
               </li>
